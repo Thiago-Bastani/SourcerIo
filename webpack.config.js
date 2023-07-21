2f43593e4485f04b8ec5b2b5aab0df15a6734078
Thiago Bastani <thiagobastani.tb@gmail.com>
2023-07-21 18:23:45 -0300

    setting webpack.js

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: __dirname + "/src/main.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "main.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+            plugins: ["@babel/plugin-syntax-jsx"],
+          },
+        },
+      },
+    ],
+  },
+};
