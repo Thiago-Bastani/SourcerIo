56adaf83254e042764786f12722a23ba237b9fa7
Thiago Bastani <thiagobastani.tb@gmail.com>
2023-07-23 14:47:48 -0300

    update Player import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 /*** Entry point */
 
-import Player from "../Domain/PlayerFactory";
+import Player from "../Domain/Player";
 
-let p1 = new Player("Nome");
+let p1 = new Player("Thiago");
+let p2 = new Player("Matheus");
 console.log(p1.name);
+console.log(p2.name);
